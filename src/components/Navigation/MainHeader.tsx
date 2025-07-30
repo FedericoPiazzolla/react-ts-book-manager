cb5c73783b1273a69311c8f1432b1cdc560cc0f0
FedericoPiazzolla <Federicop257@gmail.com>
2025-07-30 19:22:08 +0200

    add: custom input and custom button component

---
+++ src/components/Navigation/MainHeader.tsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+import Button from "../Ui/Button.tsx";
+
+export default function MainHeader() {
+  return (
+    <>
+      <header id="main-header">
+        <h1>ReactMentoring</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "")}
+                end>
+                Our Mission
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/sessions"
+                className={({ isActive }) => (isActive ? "active" : "")}>
+                Browse Sessions
+              </NavLink>
+            </li>
+            <li>
+              <Button>Upcoming Sessions</Button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
